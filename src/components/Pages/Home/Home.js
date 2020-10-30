0b1869bf9319687bc05799c0f17011e3e2c8a74b
DucSkii <ducvietdao@live.co.uk>
2020-10-30 12:31:40 +0000

    home page styled

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,15 +1,28 @@
 import React from 'react'
 
 import Navigation from '../../Navigation/Navigation'
+import Model from '../../images/websiteModel.jpg'
+import Flower from '../../images/flower.png'
 
 import './Home.scss'
 
 const Home = () => {
 
   return (
-    <div>
+    <div className="home-container">
       <Navigation />
-      <h1>Home</h1>
+      
+      <div className="home-title" >Duck</div>
+      <div className="home-subTitle" >Watches and Jewellery</div>
+      <div className="home-description">
+        <div className="home-description-text">Finest Quality</div>
+        <div className="home-description-text">Dreamy</div>
+        <div className="home-description-text">Just for her</div>
+      </div>
+      <div className="home-model-wrapper" >
+        <img className="home-flower" src={Flower} />
+        <img className="home-model" src={Model} />
+      </div>
     </div>
   )
 }
