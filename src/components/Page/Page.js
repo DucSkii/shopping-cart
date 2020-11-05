e664301b06a8be9ea37aa3e5edb125da7cd21333
DucSkii <ducvietdao@live.co.uk>
2020-11-05 11:32:39 +0000

    scroll to top button added

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,17 +1,38 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons'
 import './Page.scss'
 
 const Page = ({ className, title, gender, children }) => {
+
+  const [ showScroll, setShowScroll ] = useState(false)
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400){
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 400){
+      setShowScroll(false)
+    }
+  }
+
+  const scrollTop = () =>{
+    window.scrollTo({top: 0, behavior: 'smooth'})
+  }
+
+  window.addEventListener('scroll', checkScrollTop)
+
   return (
-    <div className={`page ${className}`}>
-      <div className="page-title">{title}</div>
-      <div className="page-subTitle">{gender}</div>
-      <div className="page-count">{children.length} items found</div>
-      <div className="page-items">
-        {children}
+    <>
+      <div className="page-scrollIcon" onClick={scrollTop} style={{display: showScroll ? 'flex' : 'none'}}><FontAwesomeIcon icon={faArrowCircleUp} size='4x' /></div>
+      <div className={`page ${className}`}>
+        <div className="page-title">{title}</div>
+        <div className="page-subTitle">{gender}</div>
+        <div className="page-count">{children.length} items found</div>
+        <div className="page-items">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/components/Pages/Cart/Cart.js
+++ src/components/Pages/Cart/Cart.js
@@ -3,11 +3,14 @@ import Navigation from '../../Navigation/Navigation'
 import CartItems from '../../Items/CartItems/CartItems'
 import {useStateValue} from '../../../context/StateContext'
 import {getCartSubTotal, getCartDeliveryTotal} from '../../../reducers/stateReducer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons'
 import './Cart.scss'
 
 const Cart = () => {
 
   const [{cartList}, dispatch ] = useStateValue()
+  const [ showScroll, setShowScroll ] = useState(false)
   const [ cartCount, setCartCount ] = useState(0)
 
   useEffect(() => {
@@ -37,43 +40,60 @@ const Cart = () => {
     return grandTotal
   }
 
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400){
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 400){
+      setShowScroll(false)
+    }
+  }
+
+  const scrollTop = () =>{
+    window.scrollTo({top: 0, behavior: 'smooth'})
+  }
+
+  window.addEventListener('scroll', checkScrollTop)
+
   return (
-    <div className="cartPage-container">
-      <Navigation />
-      <div className="cartPage">
-        <div className="cartPage-cart">
-          <div className="cartPage-title">Your Cart ({cartCount})</div>
-          <div className="cartPage-title-line"/>
-          <CartItems />
-        </div>
-        <div className="cartPage-total">
-          <div className="cartPage-total-title">Your Order</div>
-          <div className="cartPage-total-title-line" />
-          <div className="cartPage-total-subTotal">
-            <div className="cartPage-total-subTotal-text">SubTotal</div>
-            <div className="cartPage-total-subTotal-cost">£{getCartSubTotal(cartList)}</div>
+    <>
+      <div className="page-scrollIcon" onClick={scrollTop} style={{display: showScroll ? 'flex' : 'none'}}><FontAwesomeIcon icon={faArrowCircleUp} size='4x' /></div>
+      <div className="cartPage-container">
+        <Navigation />
+        <div className="cartPage">
+          <div className="cartPage-cart">
+            <div className="cartPage-title">Your Cart ({cartCount})</div>
+            <div className="cartPage-title-line"/>
+            <CartItems />
           </div>
-          <div className="cartPage-total-shipping">
-            <div className="cartPage-total-shipping-text">Estimated Shipping</div>
-            <div className="cartPage-total-shipping-cost">{displayDeliveryTotal(cartList)}</div>
-          </div>
-          <div className="cartPage-total-savings">
-            <div className="cartPage-total-savings-text">Savings</div>
-            <div className="cartPage-total-savings-cost">-£{getSavingsTotal(cartList)}</div>
-          </div>
-          <div className="cartPage-total-title-line" />
-          <div className="cartPage-total-grandTotal">
-            <div className="cartPage-total-grandTotal-text">Total</div>
-            <div className="cartPage-total-grandTotal-cost">£{getGrandTotal(cartList)}</div>
-          </div>
-          <div className="cartPage-total-savings-description">
-            <div className="cartPage-total-savings-description-normal">You saved</div>
-            <div className="cartPage-total-savings-description-bold">10% Off </div>
-            <div className="cartPage-total-savings-description-normal">your items</div>
+          <div className="cartPage-total">
+            <div className="cartPage-total-title">Your Order</div>
+            <div className="cartPage-total-title-line" />
+            <div className="cartPage-total-subTotal">
+              <div className="cartPage-total-subTotal-text">SubTotal</div>
+              <div className="cartPage-total-subTotal-cost">£{getCartSubTotal(cartList)}</div>
+            </div>
+            <div className="cartPage-total-shipping">
+              <div className="cartPage-total-shipping-text">Estimated Shipping</div>
+              <div className="cartPage-total-shipping-cost">{displayDeliveryTotal(cartList)}</div>
+            </div>
+            <div className="cartPage-total-savings">
+              <div className="cartPage-total-savings-text">Savings</div>
+              <div className="cartPage-total-savings-cost">-£{getSavingsTotal(cartList)}</div>
+            </div>
+            <div className="cartPage-total-title-line" />
+            <div className="cartPage-total-grandTotal">
+              <div className="cartPage-total-grandTotal-text">Total</div>
+              <div className="cartPage-total-grandTotal-cost">£{getGrandTotal(cartList)}</div>
+            </div>
+            <div className="cartPage-total-savings-description">
+              <div className="cartPage-total-savings-description-normal">You saved</div>
+              <div className="cartPage-total-savings-description-bold">10% Off </div>
+              <div className="cartPage-total-savings-description-normal">your items</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
